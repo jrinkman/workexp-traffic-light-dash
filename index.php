fe41407e5e4d6926199a7ac00b762b4f356ab8e6
Jack Beringen <jack.ber.private@gmail.com>
2017-09-26 15:46:12 +1000

    Card PHP looping demo

--- index.php
+++ index.php
@@ -8,39 +8,41 @@
 
 		<!-- Compiled and minified JavaScript -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+		<style>
+			.horizontal {
+		        display: -webkit-flex;
+		        display: -ms-flexbox;
+		        display: flex;
+		    }
+		</style>
 	</head>
 	<body>
 		<?php
-			//include_once('php/api.php');
-			$curl_handle=curl_init();
+			include_once('php/api.php');
+			$api_templates_callback = scApi('https://api.safetyculture.io/templates/search?owner=me');
 
-			//Set the request headers
-			curl_setopt($curl_handle, CURLOPT_URL, 'https://api.safetyculture.io/templates/search?owner=me');
-			curl_setopt($curl_handle, CURLOPT_HTTPHEADER, Array('Authorization: Bearer bc7285c55f83815975d7182d2103f815a7f9c347ce8c692e2bd92882d5877f7d'));
-			curl_setopt($curl_handle, CURLOPT_USERAGENT, 'SC API CLIENT');
-			curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER,1);
-			
-			//Execute the cURL request
-			$query = curl_exec($curl_handle);
-			curl_close($curl_handle);
+			foreach ($api_templates_callback->templates as $template) {
+				echo("
+					<div class='row'>
+				      <div class='col s6 m3'>
+				        <div class='card horizontal blue-grey darken-1'>
+				          <div class='card-content white-text'>
+				            <span class='card-title'>" . $template->name . "</span>
+				            <p>I am a very simple card. I am good at containing small bits of information.
+				            I am convenient becaus</p>
+				          </div>
+				          <div class='card-action white-text'>
+				          	SAFEEEEE
+				            <!--<a href='#''>This is a link</a>
+				            <a href='#'>This is a link</a>-->
+				          </div>
+				        </div>
+				      </div>
+				    </div>
+					");
 
-			echo($query);
+			}
 		?>
-    <div class="row">
-      <div class="col s6 m3">
-        <div class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <span class="card-title">Card Title</span>
-            <p>I am a very simple card. I am good at containing small bits of information.
-            I am convenient becaus</p>
-          </div>
-          <div class="card-action white-text">
-          	SAFEEEEE
-            <!--<a href="#">This is a link</a>
-            <a href="#">This is a link</a>-->
-          </div>
-        </div>
-      </div>
-    </div>
 	</body>
 </html>
---
+++ php/api.php
@@ -0,0 +1,19 @@
+<?php
+	function scApi($endpoint){
+		//Initialize the cURL handle
+		$curl_handle=curl_init();
+
+		//Set the request headers
+		curl_setopt($curl_handle, CURLOPT_URL, $endpoint);
+		curl_setopt($curl_handle, CURLOPT_HTTPHEADER, Array('Authorization: Bearer bc7285c55f83815975d7182d2103f815a7f9c347ce8c692e2bd92882d5877f7d'));
+		curl_setopt($curl_handle, CURLOPT_USERAGENT, 'SC API CLIENT');
+		curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER,1);
+		
+		//Execute the cURL request
+		$query = curl_exec($curl_handle);
+		curl_close($curl_handle);
+
+		//Return the decoded query
+		return json_decode($query);
+	}
+?>
