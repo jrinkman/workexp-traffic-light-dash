7253381172ac659fff543d9c5b6ec3ecd8ab6785
Jack Beringen <jack.ber.private@gmail.com>
2017-09-26 21:34:18 +1000

    Decreased the opacity of the 'created at' and 'modified at' text, also put them into seperate <p> tags

--- index.php
+++ index.php
@@ -50,8 +50,8 @@
 			        		<div class='card blue-grey darken-1 hoverable'>
 			          			<div class='card-content white-text'>
 				            		<span class='card-title truncate'>" . $template->name . "</span>
-				            		<p><strong>Created At: </strong>" . date('Y/m/d h:i A', strtotime($template->created_at)) . "</p>
-				            		<p><strong>Last Modified: </strong>" . date('Y/m/d h:i A', strtotime($template->created_at)) . "</p>
+				            		<p><strong>Created At: </strong></p><p style='opacity: 0.6'>" . date('Y/m/d h:i A', strtotime($template->created_at)) . "</p>
+				            		<p><strong>Last Modified: </strong></p><p style='opacity: 0.6'>" . date('Y/m/d h:i A', strtotime($template->modified_at)) . "</p>
 			            		</div>
 					          	<div class='card-action white-text'>
 					            	<a href='#'>View Audits</a>
