319bd8c65e65442483a309338469e45f86cebab3
Jack Beringen <jack.ber.private@gmail.com>
2017-09-27 16:58:41 +1000

    Initial audit summary implementation

--- audits/index.php
+++ audits/index.php
@@ -33,66 +33,72 @@
 								");
 							}
 
-							$status_text = "";
-							$percentage = ($api_audit->audit_data->score_percentage) / 100;
+							$status_text = "Undefined";
+							$status_colour = "#000000";
+							$perc_raw = $api_audit->audit_data->score_percentage;
+							$percentage = $perc_raw / 100;
+
+							if($perc_raw < 50){
+								$status_text = "Critical";
+								$status_colour = "#e53935";
+							} elseif($perc_raw >= 50){
+								$status_text = "Needs Attention";
+								$status_colour = "#ffca28";
+							} elseif($perc_raw > 80){
+								$status_text = "Good";
+								$status_colour = "#8bc34a";
+							}
+
 							echo("
 								<div class='audit'>
-									<div id='" . "' class='auditperc left'></div>
+									<div id='" . $api_audit->audit_id . "' class='auditperc left'></div>
 									<div class='left auditinfo'>
-										<h4>Audit Name</h4>
+										<h4>" . $status_text . "</h4>
 										<h5>Audited By</h5>
 										<h5>Testing</h5>
 									</div>
 								</div>
+								<script>
+									var bar = new ProgressBar.Circle('#" . $api_audit->audit_id . "', {
+										color: '" . $status_colour . "',
+										// This has to be the same size as the maximum width to
+										// prevent clipping
+										strokeWidth: 3,
+										trailWidth: 3,
+										easing: 'easeInOut',
+										duration: 1500,
+										text: {
+											autoStyleContainer: true
+										},
+										from: { color: '#aaa', width: 3 },
+										to: { color: '" . $status_colour . "', width: 3 },
+										// Set default step function for all animate calls
+										step: function(state, circle) {
+											circle.path.setAttribute('stroke', state.color);
+											circle.path.setAttribute('stroke-width', state.width);
+
+											var value = Math.round(circle.value() * 100);
+											if (value === 0) {
+												circle.setText('');
+											}
+											else {
+												circle.setText(value + '%');
+											}
+										}
+									});
+
+									bar.text.style.fontFamily = 'Roboto';
+									bar.text.style.fontSize = '2rem';
+
+									bar.animate(" . $percentage . ");  // Number from 0.0 to 1.0
+								</script>
+
 								<div class='divider'></div>
 							");
 						}
 					}
 				?>
-				<div class='audit'>
-					<div id='audittest' class='auditperc left'></div>
-					<div class='left auditinfo'>
-						<h4>Audit Name</h4>
-						<h5>Audited By</h5>
-						<h5>Testing</h5>
-					</div>
-				</div>
-
 			</div>
-			<script>
-				var bar = new ProgressBar.Circle('#audittest', {
-					color: '#aaa',
-					// This has to be the same size as the maximum width to
-					// prevent clipping
-					strokeWidth: 3,
-					trailWidth: 3,
-					easing: 'easeInOut',
-					duration: 1500,
-					text: {
-						autoStyleContainer: true
-					},
-					from: { color: '#aaa', width: 3 },
-					to: { color: '#333', width: 3 },
-					// Set default step function for all animate calls
-					step: function(state, circle) {
-						circle.path.setAttribute('stroke', state.color);
-						circle.path.setAttribute('stroke-width', state.width);
-
-						var value = Math.round(circle.value() * 100);
-						if (value === 0) {
-							circle.setText('');
-						}
-						else {
-							circle.setText(value + '%');
-						}
-					}
-				});
-
-				bar.text.style.fontFamily = 'Roboto';
-				bar.text.style.fontSize = '2rem';
-
-				bar.animate(0.8);  // Number from 0.0 to 1.0
-			</script>
 		</div>
 	</body>
 </html>
